2b01db17f65d40b46040b117aecf9b40e747b4d2
Treysgit <154331821+Treysgit@users.noreply.github.com>
2025-08-22 16:37:28 -0500

    Completed the core logic of the put-call parity arbitrage checker.

--- arbitrage.py
+++ arbitrage.py
@@ -2,16 +2,31 @@
 
 
 class Arbitrage:
-
+    
+    # min_diff defaults to 0.15 if user doesn't provide a minimum difference
     @staticmethod
-    def put_call_parity(s, p, c, k): 
-        min_dif = 0.15
+    def put_call_parity(s_disc, p, c, k_disc, min_diff): 
 
         # Calculate "same payoff"
-        left = s + p
-        right = c + k
+        diff = Arbitrage.same_payoff(s_disc, p, c, k_disc)
 
         # Check for arbitrage opportunity
-        if 
+        if diff >= min_diff:
+            print(f"Sell side: stock and put.\nShort the stock by selling now +${s_disc}, write and sell put +${p}.\n"
+                  f"Buy side: call and bond.\nBuy the call -${c}, invest in the bond (PV of strike) -${k_disc}\n"
+                  f"Locked in arbitrage: {s_disc} + {p} - {c} - {k_disc} = +${diff}")
+            
+        elif diff <= -min_diff:
+            print(f"Sell side: call and bond.\nSell bond (PV of strike) +${k_disc}, write and sell call +${c}.\n"
+                  f"Buy side: stock and put.\nBuy the stock -${s_disc}, buy the put -${p}\n"
+                  f"Locked in arbitrage: {c} + {k_disc} - {s_disc} - {p} = +${-diff}")
+        else:
+            print(f"The same payoff relationship holds between the variables, leaving no put-call parity arbitrage opportunity.")
+
+    @staticmethod
+    def same_payoff(s_disc, p, c, k_disc):
+        left = s_disc + p
+        right = c + k_disc
+        return left - right
 
         
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discount
+import arbitrage
 
 def main():
     print("This program checks for put-call parity arbitrage.\n"
@@ -16,14 +17,22 @@ def main():
     t = float(input("T: "))
     q = float(input("q: "))
     r = float(input("r: "))
+    flag = str(input("Do you want to enter a minimum difference "
+                       "for the put-call parity check (y/n): "))
+    if flag == "y" or flag == "Y":
+        min_diff = float(input("Enter minimum difference: "))
+    else:
+        min_diff = 0.15 # default
 
     # Discount stock at expiration to present value - i.e. the value of not being the holder during the dividend period
     s_disc = discount.Discount.principal_discount(s, q, t)
 
     # Discount strike at expiration to present value - i.e. the value that can grow to the full amount at expiration via risk-free rate instrument
-    k_disc = discount.Discount.principal_discount(k, r, t)
+    k_disc = discount.Discount.principal_discount(k, r, t) 
 
-    parity_params = {s_disc, p, c, k_disc}
+    arbitrage.Arbitrage.put_call_parity(s_disc, p, c, k_disc, min_diff)
+
+
 
 
 if __name__ == "__main__":
